5919f96d2a1fef02af5151bf130be3ac5e6e4819
Elena Dokrai <elenkalo@ucm.es>
2017-11-13 19:28:26 +0100

    Servicio web creado con Django

---
+++ django/rest/views.py
@@ -0,0 +1,19 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+from rest_framework import generics
+
+
+# Create your views here.
+class SnippetList(generics.ListCreateAPIView):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
